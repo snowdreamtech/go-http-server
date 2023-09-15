4d8ec1a915a312eaf91f2cedd90a87e9265bdc0b
sn0wdr1am <sn0wdr1am@qq.com>
2023-09-15 20:23:42 +0800

    fix: comparison of function To16 != nil is always true

--- pkg/net/net.go
+++ pkg/net/net.go
@@ -51,7 +51,7 @@ func GetAvailableIPS() []string {
 	for _, addr := range addr {
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			// check if IPv4 or IPv6 is not nil
-			if ipnet.IP.To4() != nil || ipnet.IP.To16 != nil {
+			if ipnet.IP.To4() != nil || ipnet.IP.To16() != nil {
 				// print available addresses
 				ips = append(ips, ipnet.IP.String())
 			}
